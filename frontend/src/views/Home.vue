eb0ac492266527ff2aa7662808a84ca4e320266f
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-07-25 19:33:06 +0300

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -56,7 +56,7 @@
             class="bag-item"
             data-test="bag-card"
           />
-          <div class="add-item-button" @click="navigateToAddCard">
+          <div class="add-item-button" @click="navigateToAddItem">
             <div class="add-item-inner">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <line x1="12" y1="5" x2="12" y2="19"></line>
