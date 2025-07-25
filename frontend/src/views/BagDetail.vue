fd9bc2cadfab77e05ff9020cbed7dc46c8b58862
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-07-25 17:25:52 +0300

    Update BagDetail.vue

--- frontend/src/views/BagDetail.vue
+++ frontend/src/views/BagDetail.vue
@@ -41,9 +41,14 @@
         
         <div class="price-section">
           <div class="price">{{ bag.price }}₽</div>
-          <a href="https://t.me/kurorooooo" class="buy-button" target="_blank">
-            Купить ✅
-          </a>
+          <div href="https://t.me/kurorooooo" class="buy-button" target="_blank">
+            <a>
+                КУПИТЬ
+            </a>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" class="telegram-icon">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"></path>
+            </svg>
+          </div>
         </div>
       </div>
     </div>
@@ -241,21 +246,22 @@ export default {
 }
 
 .buy-button {
-  display: inline-block;
-  padding: 15px 30px;
-  background: rgba(255, 255, 255, 0.25);
-  backdrop-filter: blur(10px);
-  -webkit-backdrop-filter: blur(10px);
-  border-radius: 8px;
-  border: 1px solid rgba(255, 255, 255, 0.3);
-  color: #423125;
-  text-decoration: none;
-  font-family: 'Noto Serif TC', 'Noto Serif', serif;
-  font-size: 1.5rem;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  transition: all 0.3s ease;
-  position: relative;
-  overflow: hidden;
+    display: inline-block;
+    padding: 10px 20px;
+    background: rgba(255, 255, 255, 0.25);
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(10px);
+    border-radius: 8px;
+    border: 1px solid rgba(255, 255, 255, 0.3);
+    color: #423125;
+    text-decoration: none;
+    font-family: 'Noto Serif TC', 'Noto Serif', serif;
+    font-size: 1.5rem;    
+    font-weight: 1000;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    transition: all 0.3s ease;
+    position: relative;
+    overflow: hidden;
 }
 
 .buy-button:hover {
