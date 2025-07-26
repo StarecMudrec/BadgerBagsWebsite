943041fd3f9807db3bb73c435e057a0aebc190dc
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-07-26 13:02:25 +0300

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -57,7 +57,7 @@
             data-test="bag-card"
           />
           <div style="margin: 20px; display: flex; flex-direction: column; align-items: center;">
-            <div class="add-item-button" @click="navigateToAddCard" data-test="add-item-button">
+            <div class="add-item-button" @click="navigateToAddItem" data-test="add-item-button">
               <div class="add-item-inner">
                 <svg class="plus-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M12 5V19M5 12H19" stroke="#423125" stroke-width="2" stroke-linecap="round"/>
