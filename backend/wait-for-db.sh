d2b17a49f024e1d14dcf5c676dab8a75ba8551ac
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-07-30 14:52:53 +0300

    asd

--- backend/wait-for-db.sh
+++ backend/wait-for-db.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+
+# Wait for PostgreSQL to be ready
 until pg_isready -h db -U postgres -d badgerbags; do
-  echo "Waiting for db..."
+  echo "Waiting for PostgreSQL..."
   sleep 2
 done
+
+# Run migrations
 flask db upgrade
-exec "$@"
+
+# Start the main application
+exec gunicorn --bind 0.0.0.0:8000 wsgi:app
