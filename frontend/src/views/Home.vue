eaa2b8cc55e129d377c49ee6fcbc55172d05f7c5
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-08-04 19:54:53 +0300

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -48,9 +48,7 @@
           <span v-if="isDeleting">Deleting...</span>
           <span v-else>
             {{ selectedBags.size }} 
-            <svg class="trash-icon" viewBox="0 0 24 24">
-              <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
-            </svg>
+            <i class="bi bi-trash"></i>
           </span>
         </button>
       </div>
