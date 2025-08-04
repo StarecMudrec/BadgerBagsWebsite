917f3680f688c2d20f3dc367c52da6016be7c5be
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-08-04 16:58:27 +0300

    Update BagCard.vue

--- frontend/src/components/BagCard.vue
+++ frontend/src/components/BagCard.vue
@@ -80,7 +80,6 @@ export default {
   box-sizing: border-box;
   border: 4px solid transparent;
   cursor: pointer;
-  overflow: hidden; /* Add this to contain the overlay */
 }
 
 .bag-card.raised {
@@ -108,13 +107,23 @@ export default {
 .bag-card.selected::before {
   content: '';
   position: absolute;
-  top: -4px; /* Adjust to account for border */
-  left: -4px;
-  right: -4px;
-  bottom: -4px; /* Extend to cover price tag */
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%; /* Cover entire card */
   background-color: rgba(255, 42, 42, 0.24);
-  z-index: 1; /* Increase z-index to cover price */
+  z-index: 1;
   filter: blur(4px);
+  /* Create 'cutout' for price tag */
+  clip-path: polygon(
+    0 0, 
+    100% 0, 
+    100% calc(100% - 40px), 
+    calc(100% - 40px) calc(100% - 40px),
+    calc(100% - 80px) 100%,
+    40px 100%,
+    0 calc(100% - 40px)
+  );
 }
 
 .bag-card.selected-animation {
