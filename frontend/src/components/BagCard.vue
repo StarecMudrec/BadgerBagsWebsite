e608843ad80d793e77bbbec9f0821ec108944ac7
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-08-04 16:17:49 +0300

    Update BagCard.vue

--- frontend/src/components/BagCard.vue
+++ frontend/src/components/BagCard.vue
@@ -170,9 +170,9 @@ export default {
   transition: transform 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
 }
 
-.bag-card:hover .bag-image {
+/* .bag-card:hover .bag-image {
   transform: translateY(-5px);
-}
+} */
 
 .bag-price {
   font-weight: bold;
