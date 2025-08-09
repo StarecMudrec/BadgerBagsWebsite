308ab5ea6a4c2134a07fda5d64485a045dcd875b
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-08-09 17:44:38 +0300

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -30,7 +30,7 @@
       <!-- Update the confirmation dialog template in Home.vue -->
       <div v-if="showDeleteConfirmation" class="confirmation-dialog-overlay">
         <div class="confirmation-dialog" :class="{ 'processing': isDeleting }">
-          <div v-if="isDeleting">
+          <div v-if="!isDeleting">
             <!-- Normal confirmation content -->
             <h3>Подтвердите удаление</h3>
             <p>Вы уверены, что хотите удалить выбранные товары? Это действие невозможно отменить.</p>
@@ -45,7 +45,7 @@
           </div>
           
           <!-- Loading state -->
-          <div class="deleting-state" v-if="!isDeleting">
+          <div v-else class="deleting-state">
             <div class="deleting-spinner"></div>
             <div class="deleting-text">Удаляем...</div>
           </div>
