0472e45ef3e36aae2171e93b22a922271ed4f303
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-08-11 18:40:38 +0300

    Update BagDetail.vue

--- frontend/src/views/BagDetail.vue
+++ frontend/src/views/BagDetail.vue
@@ -67,7 +67,12 @@
 
           <div class="editable-field" v-if="!editingDescription">
             <p class="section-text">{{ bag.description || 'Здесь будет находиться информация о сумке' }}</p>
-            <span class="edit-icon" @click="editDescription">✏️</span>
+            <span class="edit-icon" @click="editDescription">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+              </svg>
+            </span>
           </div>
           <div class="editable-field" v-else>
             <textarea v-model="descriptionInput" class="edit-input"></textarea>
@@ -80,7 +85,12 @@
           <div class="price-section">
             <div class="price-container" v-if="!editingPrice">
               <div class="price">{{ bag.price }}₽</div>
-              <span class="edit-icon" @click="editPrice">✏️</span>
+              <span class="edit-icon" @click="editPrice">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                  <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                </svg>
+              </span>
             </div>
             <div class="price-container" v-else>
               <input type="number" v-model.number="priceInput" class="edit-input" />
